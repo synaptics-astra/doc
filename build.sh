3d3865156f08f2f84839d0ac5da36ceec2647ecc
Lorenzo Keller <lorenzo.keller@synaptics.com>
2025-05-22 16:00:58 +0200

    Update theme to synaptics-sphinx-theme

--- build.sh
+++ build.sh
@@ -4,14 +4,4 @@ set -e
 
 ROOT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
-if [[ ! -d _build/actions ]] ; then
-   mkdir -p _build
-
-   git clone https://github.com/syna-astra-dev/action-doc-publish.git ${ROOT_DIR}/_build/actions
-fi
-
-echo Building docker...
-
-IMAGE_ID=$(docker build -q ${ROOT_DIR}/_build/actions/doc-build)
-
-docker run --rm -e GITHUB_REF -v /:/host/ -w /host/${ROOT_DIR}/ ${IMAGE_ID}
+docker run -e GITHUB_REF -e GITHUB_REPOSITORY --rm -v /:/host/ -w /host/${ROOT_DIR}/ ghcr.io/syna-astra-dev/synaptics-sphinx-theme/builder:latest
--- conf.py
+++ conf.py
@@ -1,56 +1,11 @@
-import os
-
 # setup documentation metadata
 project = 'Synaptics Astra SDK User Guide'
 copyright = '2023 - 2025, Synaptics'
 author = 'Synaptics'
-release = os.environ.get('RELEASE_VER', 'git-main')
 
-html_static_path = ['_static']
+# version of the software being documented
+release = '1.6.1'
 
-exclude_patterns = ["README.rst", "org-docs/**"]
-
-extensions = [
-    'sphinx_rtd_theme'
-]
-
-html_theme = "sphinx_rtd_theme"
+extensions = [ 'synaptics_sphinx_theme' ]
 
 html_logo = "images/logo_full_white.png"
-
-html_theme_options = {
-  'logo_only': True,
-  'style_nav_header_background': '#007dc3'
-}
-
-html_context = {
-  'display_github': True,
-  'github_repo': 'syna-astra-dev/doc',
-  'github_version': 'main',
-  'conf_py_path': '/',
-  'version': release
-}
-
-
-# this code is used to substitute #xx# variables anywhere in the source, even
-# inside literal blocks and code blocks
-
-def preprocess_variables(app, docname, source):
-    for varname, value in app.config.preprocessor_variables.items():
-        source[0] = source[0].replace(varname, value)
-
-
-preprocessor_variables = {
-    "#release#": release
-}
-
-
-def setup(app):
-
-    # add support for pre-processing
-    app.add_config_value('preprocessor_variables', {}, True)
-    app.connect('source-read', preprocess_variables)
-
-    # add custom theme overrides and javascript
-    app.add_css_file('css/synaptics.css')
-    app.add_js_file('js/synaptics.js')
